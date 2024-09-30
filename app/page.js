1b94db4f46eef056287c7225dd733f5f1410e09c
Hybola <keeratipong.b@gmail.com>
2024-09-30 17:07:58 +0700

    lecture 424: RootLayout

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
+export const metadata = {
+  title: "The Wild Oasis",
+};
 
 export default function page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise.</h1>
       <Link href="/cabins">Explore luxury cabins</Link>
     </div>
