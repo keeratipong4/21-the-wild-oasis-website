c23450b800050d5e43b0b6f1229387a1eecb3527
Hybola <keeratipong.b@gmail.com>
2024-10-12 16:53:51 +0700

    lecture 456

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -9,9 +9,9 @@ export async function generateMetadata({ params }) {
 }
 export async function generateStaticParams() {
   const cabins = await getCabins();
-  const ids = cabins.map((cabin) => {
-    cabinId: cabin.id;
-  });
+  const ids = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
   return ids;
 }
 
