76632f033779316bfb53006f0165d554463e6c3b
Hybola <keeratipong.b@gmail.com>
2024-10-16 17:04:26 +0700

    lecture 459

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,8 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
-
+export const revalidate = 3600;
+// export const revalidate = 15;
 export const metadata = {
   title: "cabins",
 };
