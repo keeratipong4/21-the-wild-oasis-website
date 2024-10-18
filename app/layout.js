60539ac9d0fbf01f1cc6205410d627a4ff73be87
Hybola <keeratipong.b@gmail.com>
2024-10-18 17:54:10 +0700

    lecture 464

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-900 text-primary-100 min-h-screen flex flex-col relative`}>
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
         <Header />
         <div className="flex-1 px-8 py-12 grid">
           <main className="mx-auto max-w-7xl w-full">{children}</main>
