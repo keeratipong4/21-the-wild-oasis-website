6b9232be09b63f570af0d38a104356d4a3b1ea00
Hybola <keeratipong.b@gmail.com>
2024-10-20 16:47:52 +0700

    lecture 468: select reservation dates using Context API

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "@/app/_components/ReservationReminder";
 export const revalidate = 3600;
 // export const revalidate = 15;
 export const metadata = {
@@ -29,6 +30,7 @@ export default function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ const josefin = Josefin_Sans({
 
 import "@/app/_styles/globals.css";
 import Header from "./_components/Header";
+import { ReservationProvider } from "@/app/_components/ReservationContext";
 
 export const metadata = {
   title: {
@@ -25,7 +26,9 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="mx-auto max-w-7xl w-full">{children}</main>
+          <main className="mx-auto max-w-7xl w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
