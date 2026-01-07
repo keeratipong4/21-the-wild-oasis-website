9b1b386425e1bfabe7ba61394f80045711624dfd
Keeratipong <keeratipong.b@gmail.com>
2026-01-07 20:38:37 +0700

    debug: hello world v2 force redeploy

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -4,8 +4,9 @@ export default function Page({ params }) {
   return (
     <div className="mt-10 text-center">
       <h1 className="text-3xl font-bold mb-4 text-green-500">
-        DEBUG: Hello World. ID: {params.cabinId}
+        DEBUG: Hello World V2. ID: {params.cabinId}
       </h1>
+      <p className="text-xl">Timestamp: {new Date().toISOString()}</p>
       <p>If you see this, routing works.</p>
     </div>
   );
